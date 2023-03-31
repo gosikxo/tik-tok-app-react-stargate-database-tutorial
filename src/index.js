9ba909b37dc06372efdb91c4770fe364c6b16bba
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-03-31 15:47:51 +0200

    Add react routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './index.css'
+import { Home } from './pages/Home'
+import { Upload } from './pages/Upload'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const App = () => {
+  return (
+
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/upload' element={<Upload />} />
+    </Routes>
+
+  )
+}
+
+export default App;
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
