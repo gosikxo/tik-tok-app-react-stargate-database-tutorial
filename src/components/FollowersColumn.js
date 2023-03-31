69e7a3b0334ffdb94db50857fd42ea88a7529728
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-03-31 16:18:04 +0200

    Add followers column component

---
+++ src/components/FollowersColumn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const FollowersColumn = () => {
+    return (
+        <div className='followers-column'>
+            <h1>FollowersColumn</h1>
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FollowersColumn } from '../components/FollowersColumn'
 
 export const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <FollowersColumn />
         </div>
     )
 }
