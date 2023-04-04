80874decaff75bb0daba0e3b9816a1bdc9b0105a
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-04-04 18:30:37 +0200

    Add more structure to Home components and map through users

--- src/components/FollowersColumn.js
+++ src/components/FollowersColumn.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const FollowersColumn = () => {
     return (
         <div className='followers-column'>
-            <h1>FollowersColumn</h1>
+            
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { FollowersColumn } from '../components/FollowersColumn'
 import axios from 'axios'
+import Card from '../components/Card'
 
 export const Home = () => {
     const [users, setUsers] = useState(null)
+    let descendingUsers
 
     const addData = async () => {
         await axios.post('/.netlify/functions/addData')
@@ -11,7 +13,6 @@ export const Home = () => {
 
     const fetchData = async () => {
         const results = await axios.get('/.netlify/functions/posts')
-        console.log(results.data)
         setUsers(results.data)
     }
 
@@ -20,13 +21,34 @@ export const Home = () => {
         fetchData()
     }, [])
 
+    if (users) {
+        descendingUsers = users.sort((a, b) => a.id < b.id ? 1 : -1)
+    }
+
     return (
-        <div className='container'>
-            <FollowersColumn />
-            <div className='feed'>
-                <h1>Home</h1>
-            </div>
-            <div className='suggested-box'></div>
-        </div>
+        <>
+            {descendingUsers && (
+                <div className='container'>
+                    <FollowersColumn />
+                    <div className='feed'>
+                        {descendingUsers.map((descendingUser, index) => {
+                            <Card
+                                key={index}
+                                user={descendingUser}
+                            />
+                        })}
+                    </div>
+                    <div className='suggested-box'>
+                        <div className='section'>
+                            <div className='suggested'>
+                                <h2 className='bold'>Suggested accounts</h2>
+                                <div className='break'>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
