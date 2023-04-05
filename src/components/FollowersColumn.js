fc0314c6586e698dbd9538b23bb1b50627eca1d2
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-04-05 18:10:08 +0200

    Add followers column

--- src/components/FollowersColumn.js
+++ src/components/FollowersColumn.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import MicroCard from '../components/MicroCard'
 
-export const FollowersColumn = () => {
+export const FollowersColumn = ({topFiveFollowing}) => {
+    const users = topFiveFollowing
+
     return (
         <div className='followers-column'>
-            
+            <div className="followers-section">
+                <div className="home" />
+                <h2 className="bold red">For You</h2>
+            </div>
+            <div className="followers-section">
+                <div className="following" />
+                <h2>Following</h2>
+            </div>
+            <hr />
+            <p>Your top accounts</p>
+            {users && users.map((user, index) => (
+                <MicroCard
+                    key={index} user={user}
+                />
+            ))}
+            <hr />
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef, useMemo } from 'react'
 import { FollowersColumn } from '../components/FollowersColumn'
 import axios from 'axios'
 import Card from '../components/Card'
 
 export const Home = () => {
     const [users, setUsers] = useState(null)
-    let descendingUsers
 
     const addData = async () => {
         await axios.post('/.netlify/functions/addData')
@@ -21,14 +20,30 @@ export const Home = () => {
         fetchData()
     }, [])
 
-    if (users) {
-        descendingUsers = users.sort((a, b) => a.id < b.id ? 1 : -1)
-    }
+    const descendingUsers = useMemo(() => {
+        if (users) {
+            return users.sort((a, b) => a.id < b.id ? 1 : -1)
+        }
+        return [];
+   }, [users])
+
+   const topFiveFollowing = useMemo(() => {
+        if (users) {
+            const following = users.filter(user => user.is_followed === true)
+            const descendingFollowing = following.sort((a, b) => a.likes < b.likes ? 1 : -1)
+            return descendingFollowing.slice(0, 5)
+        }
+        return [];
+    }, [users])
+
+    
+    console.log({ users, topFiveFollowing, descendingUsers })
+
     return (
         <>
             {descendingUsers && (
                 <div className='container'>
-                    <FollowersColumn />
+                    <FollowersColumn users={topFiveFollowing} />
                     <div className='feed'>
                         {descendingUsers.map((descendingUser, index) => (
                             <Card
